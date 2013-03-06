e92002b4e1fd66440b5b1fcabd0e573a2f91536c
Mats Svensson <mats@matsemann.com>
2013-03-06 19:59:22 +0100

    Added Maven to handle dependencies, Spring Framework and set up the webapp. Moved client (Angular) to webapp directory.

---
+++ src/main/java/no/dusken/momus/controller/TestController.java
@@ -0,0 +1,28 @@
+package no.dusken.momus.controller;
+
+import no.dusken.momus.model.Shop;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Mats Svensson
+ */
+@Controller
+@RequestMapping("/str")
+public class TestController {
+
+    @RequestMapping("/r")
+    public @ResponseBody Shop getSomething2() {
+        Shop shop = new Shop();
+        shop.setId(5);
+        shop.setName("ShopName");
+
+        return shop;
+    }
+
+    @RequestMapping("redir")
+    public String goAway() {
+        return "r";
+    }
+}
