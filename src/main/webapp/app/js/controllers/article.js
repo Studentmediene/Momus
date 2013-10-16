556d7e3ee7371b285e7aeda6949074f12f3dd74c
Mathias Ose <mathias.ose@gmail.com>
2013-10-16 20:31:16 +0200

    more duumy stuff

--- src/main/webapp/app/js/controllers/article.js
+++ src/main/webapp/app/js/controllers/article.js
@@ -24,20 +24,33 @@ angular.module('momusApp.controllers')
 //            $scope.originalArticle = angular.copy($scope.article);
 //        });
 
-        // START DUMMY ARTICLES STUFF
-
+        // This is am array of dummy articles, in the future we will GET the url-specified article the proper way
         var articles = [
             {
                 content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquam lorem ullamcorper placerat elementum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam non viverra nulla. Etiam ante nunc, elementum eget porta quis, blandit vel nisi. Donec ultricies tortor at urna viverra, ut rhoncus erat fringilla. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam a scelerisque justo. Quisque ut semper odio. Duis semper ut enim sed blandit. Mauris at vulputate arcu. Etiam tortor est, tincidunt condimentum ornare eget, tristique vel lectus. Vivamus dignissim tempor arcu id tempus. Maecenas sapien tortor, viverra in pretium ac, pulvinar sed lacus. Etiam consequat, libero ut porta malesuada, metus ante tristique quam, sed suscipit massa eros vitae justo. Vivamus ac tortor ac lacus venenatis feugiat. Suspendisse tempor dapibus urna, ac dictum ipsum fermentum in.",
+                note: {
+                    content: "Hei hå"
+                },
                 journalists: [
                     { name: "Mats" },
                     { name: "Mathias" }
+                ],
+                photographers: [
+                    { name: "Bob" },
+                    { name: "Bård" }
                 ]
             },
             {
                 content: "If something like that isn't possible you can change your templateUrl to point to a partial html file that just has ng-include and then set the url in your controller using $routeParams like this:",
+                note: {
+                    content: "nå er det jul igjen"
+                },
                 journalists: [
                     { name: "Mats" },
+                    { name: "Bård" }
+                ],
+                photographers: [
+                    { name: "Steve" },
                     { name: "Mathias" }
                 ]
             }
@@ -46,8 +59,6 @@ angular.module('momusApp.controllers')
         $scope.article = articles[$routeParams.id];
         $scope.originalArticle = angular.copy($scope.article);
 
-        // END DUMMY ARTICLES STUFF
-
         $scope.articleRules = articleParserRules;
 
         $scope.$watch('article.content', function (newVal, oldVal) {
@@ -55,18 +66,22 @@ angular.module('momusApp.controllers')
         });
 
         $scope.saveArticle = function () {
-            $http.put('/api/article', $scope.article).success(function (data) {
-                $scope.article = data;
-                $scope.originalArticle = angular.copy($scope.article);
-                $scope.articleIsDirty = false;
-            })
+            $scope.originalArticle = angular.copy($scope.article);
+            $scope.articleIsDirty = false;
+//            $http.put('/api/article', $scope.article).success(function (data) {
+//                $scope.article = data;
+//                $scope.originalArticle = angular.copy($scope.article);
+//                $scope.articleIsDirty = false;
+//            })
         };
 
         // noteCtrl
-        $http.get('/api/note').success(function (data) {
-            $scope.note = data;
-            $scope.originalNote = angular.copy($scope.note);
-        });
+//        $http.get('/api/note').success(function (data) {
+//            $scope.note = data;
+//            $scope.originalNote = angular.copy($scope.note);
+//        });
+
+        $scope.note = $scope.article.note;
 
         $scope.noteRules = noteParserRules;
 
