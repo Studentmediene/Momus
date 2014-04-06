d4d77c51dd3c781e0863a68ea759a22372a8c5ef
AndersLunde <agfct@hotmail.com>
2014-04-06 21:04:40 +0200

    added articleModal and add/remove article in disposition

--- src/main/webapp/app/js/controllers/DispositionCtrl.js
+++ src/main/webapp/app/js/controllers/DispositionCtrl.js
@@ -17,58 +17,251 @@
 'use strict';
 
 angular.module('momusApp.controllers')
-    .controller('DispositionCtrl', function ($scope, $http) {
-        $scope.addArticle = function(page){
-            console.log("Add article on page "+ page.pageNr);
+    .controller('DispositionCtrl', function ($scope, $http, $routeParams, $modal) {
+        $scope.addArticle = function(){
+            console.log("TRYKKER PÅ NOE");
+//            $scope.disposition.pages[page.pageNr-1].articles.push;
         };
-        $scope.arrayen = [
+
+
+        $scope.articleModal = function (page) {
+
+            var modalInstance = $modal.open({
+                templateUrl: 'partials/disposition/articleModal.html',
+                controller: ModalInstanceCtrl,
+                resolve: {
+                    articles: function () {
+                        return $scope.articles;
+                    },
+                    page: function () {
+                        return page;
+                    }
+                }
+
+                });
+
+        };
+        var ModalInstanceCtrl = function ($scope, $modalInstance, articles, page) {
+
+            if(articles.length != 0){
+                $scope.addArticleModel = articles[0];
+            }
+            $scope.page = page;
+            $scope.articles = articles;
+            // if the page.articles is not empty, set a default value for the model
+            if($scope.page.articles.length != 0){
+                $scope.delArticleModel = $scope.page.articles[0];
+            }
+
+            $scope.addArticle = function(articleModel){
+                if(!articleModel){
+                    return;
+                }
+//                if($scope.page.articles.indexOf(articleModel) == -1){
+                var pageAricles = $scope.page.articles;
+                for(var i=0; i < pageAricles.length; i++){
+                    if(pageAricles[i].id == articleModel.id){
+                        return ;
+                    }
+                }
+                $scope.page.articles.push(articleModel);
+
+//                };
+            };
+            $scope.removeArticle = function(articleModel){
+                if(articleModel){
+                    var pageAricles = $scope.page.articles;
+                    for(var i=0; i < pageAricles.length; i++){
+                        if(pageAricles[i].id == articleModel.id){
+                            $scope.page.articles.splice(i,1);
+                            return ;
+                        }
+                    }
+
+                }
+            };
+
+
+        };
+
+//        $http.get('/api/publication/'+$routeParams.id).success(function(data) {
+//            $scope.articles = data;
+//            console.log(data);
+//        });
+        $scope.articles = [
             {
-                name:"håkon",
-                age:24
+                id: 1,
+                type:"KulturRaport",
+                name:"Fuglefrø",
+                status: "Skrives",
+                photoStatus: "tatt",
+                advertisement: false,
+                photographers: [
+                    {
+                        name:"jon",
+                        age:24
+                    },
+                    {
+                        name:"birger",
+                        age:22
+                    },
+                    {
+                        name:"olav",
+                        age:45
+                    },
+                    {
+                        name:"kåre",
+                        age:45
+                    }
+                ],
+                journalists: [
+                    {
+                        name:"håkon",
+                        age:24
+                    },
+                    {
+                        name:"bård",
+                        age:22
+                    },
+                    {
+                        name:"stian",
+                        age:45
+                    }
+                ]
             },
             {
-                name:"bård",
-                age:21
+                id: 2,
+                type:"Portrett",
+                name:"Nato Jens",
+                status: "Desk",
+                photoStatus: "lagt inn",
+                advertisement: true,
+                photographers: [],
+                journalists: [
+                    {
+                        name:"ole",
+                        age:20
+                    }
+                ]
+            },
+            {
+                id: 3,
+                type:"Miljø",
+                name:"Oljesøl",
+                status: "Skrives",
+                photoStatus: "redigeres",
+                advertisement: false,
+                photographers: [],
+                journalists: [
+                    {
+                        name:"frode",
+                        age:28
+                    }
+                ]
             }
+
         ];
-        // adding all journalists that are in a page, looping throu all articles
-        $scope.getJournalists = function(page){
-//           MER PRIKETE EN JAVA, LAGET LISTA OMIGJEN DA BLE DET NY ITEREASJON
-            return $scope.arrayen;
-        };
+
         // Test disposition
         $scope.disposition = {
-            id:0,
+            id: $routeParams.id,
             publicationNr:3,
             release_date:2014,
             pages:[
                 {
                     pageNr:1,
+                    section: "Kultur",
                     articles: [
                         {
+                            id:1,
                             type:"KulturRaport",
                             name:"Fuglefrø",
+                            status: "Skrives",
+                            photoStatus: "tatt",
+                            advertisement: false,
+                            photographers: [
+                                {
+                                    name:"jon",
+                                    age:24
+                                },
+                                {
+                                    name:"birger",
+                                    age:22
+                                },
+                                {
+                                    name:"olav",
+                                    age:45
+                                },
+                                {
+                                    name:"kåre",
+                                    age:45
+                                }
+                            ],
                             journalists: [
                                 {
                                     name:"håkon",
                                     age:24
-
+                                },
+                                {
+                                    name:"bård",
+                                    age:22
+                                },
+                                {
+                                    name:"stian",
+                                    age:45
                                 }
                             ]
                         },
                         {
+                            id: 2,
                             type:"Portrett",
-                            name:"Nato Jens"
+                            name:"Nato Jens",
+                            status: "Desk",
+                            photoStatus: "lagt inn",
+                            advertisement: true,
+                            photographers: [],
+                            journalists: [
+                                {
+                                    name:"ole",
+                                    age:20
+                                }
+                            ]
                         }
                     ]
                 },
                 {
                     pageNr:2,
+                    section: "",
                     articles: [
                         {
+                            id: 3,
                             type:"Miljø",
-                            name:"Oljesøl"
+                            name:"Oljesøl",
+                            status: "Skrives",
+                            photoStatus: "redigeres",
+                            advertisement: false,
+                            photographers: [],
+                            journalists: [
+                                {
+                                    name:"frode",
+                                    age:28
+                                }
+                            ]
                         }
+                    ]
+                },
+                {
+                    pageNr:3,
+                    section: "",
+                    articles: [
+
+                    ]
+                },
+                {
+                    pageNr:4,
+                    section: "",
+                    articles: [
+
                     ]
                 }
             ]
