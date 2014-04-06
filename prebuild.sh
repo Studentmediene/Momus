404184e9ee7e81909337ccd9d90e061c6642caab
Mats Krüger Svensson <mats@matsemann.com>
2014-04-06 23:01:08 +0200

    test favicon to see how we can replace the ugly one from jetty

--- prebuild.sh
+++ prebuild.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+# copy in production values
 cp /srv/momus/local.properties src/main/resources/local.properties
+
+# install frontend dependencies
 npm install
 bower install
+
+# build frontent
 grunt build
