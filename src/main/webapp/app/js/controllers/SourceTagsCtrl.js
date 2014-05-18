d3aaf8247be4011d3c93e45444d7a1987a7fe2ed
Mats Krüger Svensson <mats@matsemann.com>
2014-05-19 00:33:33 +0200

    cleanup of source tags manager. Will now mark unused tags instead, which made the code cleaner. Using button-loader to display feedback

--- src/main/webapp/app/js/controllers/SourceTagsCtrl.js
+++ src/main/webapp/app/js/controllers/SourceTagsCtrl.js
@@ -17,66 +17,70 @@
 'use strict';
 
 angular.module('momusApp.controllers')
-    .controller('SourceTagsCtrl', function ($scope, $http) {
+    .controller('SourceTagsCtrl', function ($scope, $http, $q) {
         $scope.tags = [];
-        $scope.unused = [];
-        $scope.showUnused = false;
-
         $scope.originalTags = [];
-        $scope.originalUnused= [];
-
-
-        $scope.isChanged = function(tag, index) {
-            if ($scope.showUnused) {
-                return tag.tag != $scope.originalUnused[index].tag;
-            } else {
-                return tag.tag != $scope.originalTags[index].tag;
-            }
-        };
-
-        $scope.updateTag = function(newTag, index) {
-            var oldTag;
-
-            if ($scope.showUnused) {
-                oldTag = $scope.originalUnused[index];
-            } else {
-                oldTag = $scope.originalTags[index];
-            }
-
-            $http.put('/api/source/tag/' + oldTag.tag, newTag).success(function(data) {
-                getTagsFromServer();
-            });
-        };
-
-        $scope.deleteTag = function(index) {
-            if (!confirm("Vil du slette denne tagen og fjerne den fra alle kilder?")) {
-                return;
-            }
-
-            var oldTag;
-
-            if ($scope.showUnused) {
-                oldTag = $scope.originalUnused[index];
-            } else {
-                oldTag = $scope.originalTags[index];
-            }
-
-            $http.put('/api/source/tag/delete', oldTag).success(function(data) {
-                getTagsFromServer();
-            });
-        };
+        $scope.isSaving = {}; // holds true/false for if the given tag is currently saving, for displaying loading button
 
         getTagsFromServer();
 
+        $scope.isChanged = function (tag, index) {
+            return tag.tag != $scope.originalTags[index].tag;
+        };
+
+        $scope.updateTag = function (newTag, index) {
+            var oldTag = $scope.originalTags[index];
+            var tagToSave = {tag: newTag.tag}; // creating a new object, as the original one may have "unused" values set
+
+            $scope.isSaving[index] = true;
+
+            $http.put('/api/source/tag/' + encodeURIComponent(oldTag.tag), tagToSave).success(function (data) {
+                oldTag.tag = newTag.tag;
+                $scope.isSaving[index] = false;
+            });
+        };
+
+        $scope.deleteTag = function (index) {
+            var tagToDelete = $scope.originalTags[index];
+            var unused = $scope.tags[index].unused;
+
+            if (!unused && !confirm("Vil du slette denne tagen og fjerne den fra alle kilder?")) {
+                return;
+            }
+
+            $http.put('/api/source/tag/delete', tagToDelete).success(function (data) {
+                // remove from array
+                $scope.originalTags.splice(index, 1);
+                $scope.tags.splice(index, 1);
+            });
+        };
+
+
         function getTagsFromServer() {
-            $http.get('/api/source/tag').success(function(data) {
+            var tagPromise = $http.get('/api/source/tag');
+            var unusedPromise = $http.get('/api/source/tag/unused');
+
+            tagPromise.success(function (data) {
                 $scope.tags = data;
                 $scope.originalTags = angular.copy(data);
             });
-            $http.get('/api/source/tag/unused').success(function(data) {
+            unusedPromise.success(function (data) {
                 $scope.unused = data;
-                $scope.originalUnused = angular.copy(data);
             });
+
+            $q.all([tagPromise, unusedPromise]).then(function () {
+                markUnusedTags($scope.tags, $scope.unused);
+            })
+        }
+
+        function markUnusedTags(allTags, unused) {
+            for (var i = 0; i < unused.length; i++) {
+                for (var j = 0; j < allTags.length; j++) {
+                    if (unused[i].tag == allTags[j].tag) {
+                        allTags[j]["unused"] = true;
+                    }
+                }
+            }
         }
 
     });
