21e00eae5c214ec30a36c43123059b210c4aa98d
Eivind Grimstad <eivind@rognene.no>
2014-09-21 22:55:37 +0200

    Added new controller and partial for viewing revisions of an article. Started formatting the interface.

--- src/main/webapp/app/js/app.js
+++ src/main/webapp/app/js/app.js
@@ -52,6 +52,13 @@ angular.module('momusApp', [
             }
         )
 
+            .when('/artikler/revisjon/:id',
+            {
+                templateUrl: 'partials/article/articleRevisionView.html',
+                controller: 'ArticleRevisionCtrl'
+            }
+        )
+
             .when('/utgaver',
             {
                 templateUrl: 'partials/publication/publicationView.html',
