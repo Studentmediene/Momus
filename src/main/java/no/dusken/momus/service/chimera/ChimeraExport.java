c5596ab74be12811a18540a9ddc6a0ad4bc57c5d
Eivind Grimstad <egrimstad95@gmail.com>
2015-02-22 20:56:13 +0100

    Added a test for conversion to json. Now the json posts both the authors as a string (field external_authors) and as a json with the ldap id (ldap_authors). Also slight changes on the html to markdown and moved the login info for Chimera to momus.properties.

--- src/main/java/no/dusken/momus/service/chimera/ChimeraExport.java
+++ src/main/java/no/dusken/momus/service/chimera/ChimeraExport.java
@@ -1,12 +1,14 @@
 package no.dusken.momus.service.chimera;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 import no.dusken.momus.model.Article;
 import no.dusken.momus.model.Person;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.codec.Base64;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
@@ -19,6 +21,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
@@ -27,13 +30,23 @@ import java.util.Set;
 public class ChimeraExport {
     Logger logger = LoggerFactory.getLogger(getClass());
 
+    @Value("${chimera.username}")
+    String username;
+
+    @Value("${chimera.password}")
+    String password;
+
+    @Value("${chimera.url}")
+    String url;
+
     public Boolean exportToChimera(Article article){
         String json = articleToJson(article);
         HttpURLConnection connection = null;
         try{
-            connection = (HttpURLConnection) new URL("http://staging.dusken.no/artikkel/api/momus/").openConnection();
+            connection = (HttpURLConnection) new URL(url).openConnection();
             //logger.info("Connected to Chimera");
-            String userpswd = "momus:hakkespett";
+
+            String userpswd = username +":" + password;
             String encoded = new String(Base64.encode(userpswd.getBytes()));
             connection.setRequestProperty("Authorization", "Basic "+encoded);
             connection.setRequestProperty("Content-Type", "application/json");
@@ -70,43 +83,61 @@ public class ChimeraExport {
         ObjectNode json = mapper.getNodeFactory().objectNode();
 
         String content = article.getContent();
-        String title = content.substring(content.indexOf("<h1>"), content.indexOf("</h1>")+5);
-        String lead = content.substring(content.indexOf("<h4>"), content.indexOf("</h4>")+5);
-        content = content.replace(title, "").replace(lead, "");
+        if(content.contains("<h1>")){
+            String title = content.substring(content.indexOf("<h1>"), content.indexOf("</h1>")+5);
+            content = content.replace(title,"");
+            json.put("headline", title.replace("<h1>","").replace("</h1>",""));
+        }
+        if(content.contains("<h4>")){
+            String lead = content.substring(content.indexOf("<h4>"), content.indexOf("</h4>")+5);
+            content = content.replace(lead,"");
+            json.put("lead", lead.replace("<h4>","").replace("</h4>", ""));
+        }
         json.put("id", article.getId());
-        json.put("headline", title.replace("<h1>","").replace("</h1>",""));
-        json.put("lead", lead.replace("<h4>","").replace("</h4>", ""));
         json.put("body", htmlToMarkdown(content));
-        json.put("authors", journalistsJson(article));
+        json.put("external_authors", getJournalistsAsString(article));
+        json.put("ldap_authors", journalistsJson(article));
 
         return json.toString();
     }
 
-    public ObjectNode journalistsJson(Article article){
-        ObjectMapper mapper = new ObjectMapper();
-        ObjectNode json = mapper.getNodeFactory().objectNode();
+    public ArrayNode journalistsJson(Article article){
+        ObjectMapper mapperArray = new ObjectMapper();
+        ArrayNode jsonArray = mapperArray.getNodeFactory().arrayNode();
 
         Set<Person> journalists = article.getJournalists();
         for(Person person : journalists){
+            ObjectMapper mapper = new ObjectMapper();
+            ObjectNode json = mapper.getNodeFactory().objectNode();
             json.put("id", person.getId());
             json.put("name", person.getFullName());
+            jsonArray.add(json);
         }
-        return json;
+        return jsonArray;
     }
 
-    public String htmlToMarkdownNew(String html){
+    public String getJournalistsAsString(Article article){
+        ArrayList<String> journs = new ArrayList<>();
+        for(Person e : article.getJournalists()){
+            journs.add(e.getFullName());
+        }
+        return journs.toString().substring(1, journs.toString().length()-1);
+    }
+
+    public String htmlToMarkdown(String html){
         final Map<String,String> reps = new LinkedHashMap<>();
-        reps.put("<p>","");
+        reps.put("<p>","\n");
         reps.put("</p>","\n");
-        reps.put("<h1>","#");
+        reps.put("<h1>","\n#");
         reps.put("</h1>","\n");
-        reps.put("<h2>","##");
+        reps.put("<h2>","\n##");
         reps.put("</h2>","\n");
-        reps.put("<h3>","###");
+        reps.put("<h3>","\n###");
         reps.put("</h3>","\n");
-        reps.put("<h4>","####");
+        reps.put("<h4>","\n####");
         reps.put("</h4>","\n");
-        reps.put("<blockquote>",">");
+        reps.put("<blockquote>","\n>");
+        reps.put("</blockquote>","\n");
         reps.put("<br>","\n");
         reps.put("<i>","*");
         reps.put("</i>","*");
@@ -125,7 +156,7 @@ public class ChimeraExport {
                 marklist.append("* " + htmllist.substring(htmllist.indexOf("<li>")+4, htmllist.indexOf("</li>"))+"\n");
                 htmllist = htmllist.replaceFirst("<li>", "").replaceFirst("</li>", "");
             }
-            html = html.substring(0,start) + marklist.toString() + html.substring(end+5);
+            html = html.substring(0,start) + "\n" + marklist.toString() + html.substring(end+5);
         }
         while(html.indexOf("<ol>") != -1){
             int start = html.indexOf("<ol>");
@@ -138,54 +169,9 @@ public class ChimeraExport {
                 htmllist = htmllist.replaceFirst("<li>","").replaceFirst("</li>","");
                 i++;
             }
-            html = html.substring(0,start) + marklist.toString() + html.substring(end+5);
+            html = html.substring(0,start) + "\n" + marklist.toString() + html.substring(end+5);
         }
         return html;
     }
 
-    public String htmlToMarkdown(String str){
-        String[] tags = {"<p>","<h1>","<h2>","<h3>","<h4>","<blockquote>","<ul>","<ol>","<i>","<br>"};
-        String[] markdown = {"\n","#","##","###","####",">","","","*","\n"};
-        for(int i = 0; i < tags.length; i++){
-            while(str.contains(tags[i])){
-                int startindex = str.indexOf(tags[i]);
-                int endindex = 0;
-                if(i != 9){
-                    endindex = str.indexOf(tags[i].substring(0, 1) + "/" + tags[i].substring(1));
-                    str = str.substring(0,startindex)+markdown[i]+str.substring(startindex,endindex)+str.substring(endindex);
-                }
-                if(i == 6){
-                    String ting = "\n"+str.substring(startindex,endindex);
-                    while(ting.contains("<li>")){
-                        int start = ting.indexOf("<li>");
-                        int end = ting.indexOf("</li>");
-                        ting = ting.substring(0,start)+"* "+ting.substring(start,end)+"\n"+ting.substring(end);
-                        ting = ting.replaceFirst("<li>","");
-                        ting = ting.replaceFirst("</li>","");
-                    }
-                    str = str.replace(str.substring(startindex,endindex),ting);
-                }else if(i==7){
-                    String ting = "\n"+str.substring(startindex,endindex);
-                    int it = 1;
-                    while(ting.contains("<li>")){
-                        int start = ting.indexOf("<li>");
-                        int end = ting.indexOf("</li>");
-                        ting = ting.substring(0,start)+it+". "+ting.substring(start,end)+"\n"+ting.substring(end);
-                        ting = ting.replaceFirst("<li>","");
-                        ting = ting.replaceFirst("</li>","");
-                        it++;
-                    }
-                    str = str.replace(str.substring(startindex,endindex),ting);
-                }
-                str = str.replaceFirst(tags[i],"");
-                if(i == 8){
-                    str = str.replaceFirst(tags[i].substring(0,1) +"/" + tags[i].substring(1),"*\n");
-                }else if(i != 9){
-                    str = str.replaceFirst(tags[i].substring(0, 1) + "/" + tags[i].substring(1),"\n");
-                }
-                //logger.info(str);
-            }
-        }
-        return str;
-    }
 }
--- src/test/java/no/dusken/momus/service/chimera/ChimeraExportTest.java
+++ src/test/java/no/dusken/momus/service/chimera/ChimeraExportTest.java
@@ -1,10 +1,14 @@
 package no.dusken.momus.service.chimera;
 
 import no.dusken.momus.model.Article;
+import no.dusken.momus.model.Person;
 import no.dusken.momus.test.AbstractTestRunner;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
@@ -27,9 +31,32 @@ public class ChimeraExportTest extends AbstractTestRunner{
                 "<li>Andre</li>" +
                 "<li>Tredje</li></ol>" +
                 "<p><b>Dette er fet skrift</b></p><br>" +
-                "<p><i>Dette er italics</i></p>");
+                "<p><i>Dette er italics</i></p>" +
+                "<blockquote>Dette er et sitat!</blockquote>");
 
 
-        assertEquals("#Dette er tittelen\n####Dette er en ingress\nDette er et avsnitt som bare er en test.\n* Dette er et element\n* Et til element\n* Enda et\n1. Første\n2. Andre\n3. Tredje\n**Dette er fet skrift**\n\n*Dette er italics*\n",chimeraExport.htmlToMarkdownNew(article.getContent()));
+        assertEquals("\n#Dette er tittelen\n\n####Dette er en ingress\n\nDette er et avsnitt som bare er en test.\n\n* Dette er et element\n* Et til element\n* Enda et\n\n1. Første\n2. Andre\n3. Tredje\n\n**Dette er fet skrift**\n\n\n*Dette er italics*\n\n>Dette er et sitat!\n",chimeraExport.htmlToMarkdown(article.getContent()));
+    }
+
+    @Test
+    public void testJsonConversion() throws Exception{
+        Article article = new Article((long) 0);
+        article.setContent("<h1>Tittel</h1><h4>Dette er en ingress</h4><p>Dette er et avsnitt</p>");
+        Person p1 = new Person((long) 0);
+        p1.setUsername("eivind");
+        p1.setFirstName("Eivind");
+        p1.setFullName("Eivind Grimstad");
+        p1.setEmail("e@post.no");
+        Person p2 = new Person((long) 1);
+        p2.setUsername("ola");
+        p2.setFirstName("Ola");
+        p2.setFullName("Ola Nordmann");
+        p2.setEmail("mail@mail.no");
+        Set<Person> journ = new HashSet<>();
+        journ.add(p1);
+        journ.add(p2);
+        article.setJournalists(journ);
+        System.out.println(article.getJournalists().size());
+        assertEquals("{\"headline\":\"Tittel\",\"lead\":\"Dette er en ingress\",\"id\":0,\"body\":\"\\nDette er et avsnitt\\n\",\"external_authors\":\"Eivind Grimstad, Ola Nordmann\",\"ldap_authors\":[{\"id\":0,\"name\":\"Eivind Grimstad\"},{\"id\":1,\"name\":\"Ola Nordmann\"}]}", chimeraExport.articleToJson(article));
     }
 }
