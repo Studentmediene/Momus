0d76c8e0146570829e9d5508029f15a90ea538fa
Christoffer Gran <cwgran@gmail.com>
2015-11-06 20:50:04 +0100

    Adding background color to review status

--- src/main/java/no/dusken/momus/model/ArticleReview.java
+++ src/main/java/no/dusken/momus/model/ArticleReview.java
@@ -30,6 +30,8 @@ public class ArticleReview {
 
     private String name;
 
+    private String color;
+
     public ArticleReview() {
     }
 
@@ -48,6 +50,10 @@ public class ArticleReview {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getColor() { return color; }
+
+    public void setColor(String color) { this.color = color; }
 
     @Override
     public boolean equals(Object o) {
