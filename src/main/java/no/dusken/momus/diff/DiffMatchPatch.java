0874e53138c2d6f2566a37c55d1dd88f395ee6c0
egrimstad <egrimstad95@gmail.com>
2016-08-28 21:59:39 +0200

    Updated copyright text on all files

--- src/main/java/no/dusken/momus/diff/DiffMatchPatch.java
+++ src/main/java/no/dusken/momus/diff/DiffMatchPatch.java
@@ -1,14 +1,11 @@
 /*
- * Diff Match and Patch
- *
- * Copyright 2006 Google Inc.
- * http://code.google.com/p/google-diff-match-patch/
+ * Copyright 2016 Studentmediene i Trondheim AS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
