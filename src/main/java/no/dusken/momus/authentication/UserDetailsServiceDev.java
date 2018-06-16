934376cce42bcd4c80d38a793ce575759c5bf50d
egrimstad <egrimstad95@gmail.com>
2018-06-16 11:57:05 +0200

    Simplify mock user login

--- src/main/java/no/dusken/momus/authentication/UserDetailsServiceDev.java
+++ src/main/java/no/dusken/momus/authentication/UserDetailsServiceDev.java
@@ -1,19 +1,29 @@
 package no.dusken.momus.authentication;
 
 import no.dusken.momus.model.Person;
+import no.dusken.momus.config.MockToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.saml.SAMLCredential;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
 
-public class UserDetailsServiceDev implements UserDetailsService{
+public class UserDetailsServiceDev implements UserDetailsService {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @Override
     public Person getLoggedInPerson() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication.isAuthenticated()) {
-            return (Person) authentication.getPrincipal();
+        if(authentication.getClass().equals(AnonymousAuthenticationToken.class)) {
+            authentication = authenticationManager.authenticate(new MockToken("eivigri"));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-        return null;
+
+        return (Person) authentication.getPrincipal();
     }
 
     @Override
