d650bfd56682c8a44cb5f1bf04f20b525ba33a6e
Eivind Grimstad <egrimstad95@gmail.com>
2018-09-17 20:30:36 +0200

    MOM-252 Add cookie service for storing recent articles

--- webapp-beta/services/app.services.ts
+++ webapp-beta/services/app.services.ts
@@ -1,15 +1,21 @@
 import * as angular from 'angular';
+import * as angularCookies from 'angular-cookies';
+
 import momResourceFactory from './momResource.factory';
 import openModal from './openModal.factory';
 import sessionService from './session.service';
 import tips, { Tip } from './tips';
+import CookieService from './cookies.service';
 
 export type RandomTip = () => Tip;
 
-export default angular.module('momusApp.services', [])
+export default angular.module('momusApp.services', [
+        angularCookies,
+    ])
     .factory('momResource', momResourceFactory)
     .factory('openModal', openModal)
     .factory('randomTip', () => () => (
         tips[Math.floor(Math.random() * tips.length)]
     ))
-    .service('sessionService', sessionService);
+    .service('sessionService', sessionService)
+    .service('cookieService', CookieService);
