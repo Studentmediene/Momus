8e0b606f6d127c98d7e9433e8ff73e56c307f91a
Eivind Grimstad <egrimstad95@gmail.com>
2018-09-20 12:34:26 +0200

    MOM-252 Add button loading directive

--- webapp-beta/app.directives.ts
+++ webapp-beta/app.directives.ts
@@ -1,17 +1,66 @@
 import * as angular from 'angular';
 
-interface Scope extends angular.IScope {
+interface AliasScope extends angular.IScope {
     [index: string]: any;
 }
 
+/* @ngInject */
+function aliasDirective(): angular.IDirective {
+    return {
+        restrict: 'A',
+        scope: true,
+        link: (scope: AliasScope, el, attrs) => {
+            const alias = attrs.alias.split(' as ');
+            scope.$watch(alias[0], () => scope[alias[1]] = scope.$eval(alias[0]));
+        },
+    };
+}
+
+interface ButtonLoadingScope extends angular.IScope {
+    buttonLoading: boolean;
+}
+
+interface ButtonLoadingAttrs extends angular.IAttributes {
+    loadingText?: string;
+    loadedText?: string;
+    hideLoadingIcons?: boolean;
+}
+
+/* @ngInject */
+function buttonLoadingDirective($timeout: angular.ITimeoutService): angular.IDirective {
+    return {
+        restrict: 'A',
+        scope: {
+            buttonLoading: '<',
+        },
+        link: (scope: ButtonLoadingScope, el, attrs: ButtonLoadingAttrs) => {
+            const originalHtml = el.html();
+            const loadingText = attrs.loadingText || 'Lagrer';
+            const loadingHtml = attrs.hideLoadingIcons
+                ? loadingText
+                : `<i class="fas fa-spinner fa-spin"></i> ${loadingText}`;
+
+            const loadedText = attrs.loadedText || 'Lagret';
+            const loadedHtml = attrs.hideLoadingIcons
+                ? loadedText
+                : `<i class="fas fa-check"></i> ${loadedText}`;
+
+            scope.$watch('buttonLoading', (isLoading: boolean) => {
+                if (isLoading) {
+                    el.html(loadingHtml);
+                    el.attr('disabled', 'true');
+                } else {
+                    el.html(loadedHtml);
+                    $timeout(() => {
+                        el.attr('disabled', null);
+                        el.html(originalHtml);
+                    }, 1000);
+                }
+            });
+        },
+    };
+}
+
 export default angular.module('momusApp.directives', [])
-    .directive('alias', (): angular.IDirective => {
-        return {
-            restrict: 'A',
-            scope: true,
-            link: (scope: Scope, el, attrs) => {
-                const alias = attrs.alias.split(' as ');
-                scope.$watch(alias[0], () => scope[alias[1]] = scope.$eval(alias[0]));
-            },
-        };
-    });
+    .directive('alias', aliasDirective)
+    .directive('buttonLoading', buttonLoadingDirective);
