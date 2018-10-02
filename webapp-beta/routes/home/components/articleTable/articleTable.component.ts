b51dad32c2718bcbe90cc2f27d668c9c0ac99291
Eivind Grimstad <egrimstad95@gmail.com>
2018-10-02 17:21:45 +0200

    MOM-252 Make navigation more responsive

--- webapp-beta/routes/home/components/articleTable/articleTable.component.ts
+++ webapp-beta/routes/home/components/articleTable/articleTable.component.ts
@@ -14,43 +14,45 @@ export default angular.module('momusApp.routes.home.articleTable', [])
         >
             Ingen artikler
         </div>
-        <table
-            ng-show="vm.articles.length > 0"
-            class="uk-table uk-table-striped uk-table-small"
-            style="font-size: 80%; margin-top: 0">
-            <thead>
-                <tr>
-                    <th>Navn</th>
-                    <th>Journalister</th>
-                    <th>Redaksjon</th>
-                    <th>Status</th>
-                    <th>Utgave</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr ng-repeat="article in vm.articles">
-                    <td>
-                        <a ui-sref="article.single({id: article.id})">{{ article.name }}</a>
-                        <span title="Antall tegn"> ({{ article.content_length }})</span>
-                    </td>
-                    <td>
-                        <ul class="uk-list person-list">
-                            <li ng-repeat="person in article.journalists">
-                                <person-widget person="person"></person-widget>
-                            </li>
-                            <li><i uk-tooltip="Ekstern">{{article.external_author}}</i></li>
-                        </ul>
-                    </td>
-                    <td ng-style="{'background-color': article.section.color}">
-                        {{ article.section.name }}
-                    </td>
-                    <td ng-style="{'background-color': article.status.color}">
-                        {{ article.status.name }}
-                    </td>
-                    <td>{{ article.publication.name }}</td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="uk-overflow-auto">
+            <table
+                ng-show="vm.articles.length > 0"
+                class="uk-table uk-table-striped uk-table-small"
+                style="font-size: 80%; margin-top: 0">
+                <thead>
+                    <tr>
+                        <th>Navn</th>
+                        <th>Journalister</th>
+                        <th>Redaksjon</th>
+                        <th>Status</th>
+                        <th>Utgave</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr ng-repeat="article in vm.articles">
+                        <td>
+                            <a ui-sref="article.single({id: article.id})">{{ article.name }}</a>
+                            <span title="Antall tegn"> ({{ article.content_length }})</span>
+                        </td>
+                        <td>
+                            <ul class="uk-list person-list">
+                                <li ng-repeat="person in article.journalists">
+                                    <person-widget person="person"></person-widget>
+                                </li>
+                                <li><i uk-tooltip="Ekstern">{{article.external_author}}</i></li>
+                            </ul>
+                        </td>
+                        <td ng-style="{'background-color': article.section.color}">
+                            {{ article.section.name }}
+                        </td>
+                        <td ng-style="{'background-color': article.status.color}">
+                            {{ article.status.name }}
+                        </td>
+                        <td>{{ article.publication.name }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
         `,
         controllerAs: 'vm',
     });
--- webapp-beta/routes/navigation/navigation.component.ts
+++ webapp-beta/routes/navigation/navigation.component.ts
@@ -1,6 +1,27 @@
 import * as angular from 'angular';
 
 import './navigation.scss';
+import { TransitionService } from '@uirouter/core';
+
+import * as UIKit from 'uikit';
+
+class NavigationCtrl implements angular.IController {
+    private navDropdown: any;
+
+    private $transitions: TransitionService;
+
+    constructor($transitions: TransitionService) {
+        this.$transitions = $transitions;
+    }
+    public $postLink() {
+        const navDropdown = UIKit.dropdown('#navbardropdown', {offset: 0, mode: 'click', delayHide: 0});
+        this.navDropdown = navDropdown;
+
+        this.$transitions.onBefore({}, () => {
+            navDropdown.hide();
+        });
+    }
+}
 
 export default angular.module('momusApp.routes.navigation.navigation', [])
     .component('navigation', {
@@ -9,6 +30,22 @@ export default angular.module('momusApp.routes.navigation.navigation', [])
             navItems: '<',
             session: '<',
         },
+        controller: NavigationCtrl,
         controllerAs: 'vm',
         template: require('./navigation.html'),
+    })
+    .component('momusLogo', {
+        template: // html
+        `
+        <a ui-sref="home" class="momus-logo uk-navbar-item" style="align-items: {{vm.align || 'end'}}">
+            <span class="uk-logo">Momus</span>
+            <span class="beta-label">beta</span>
+            <div ng-if="vm.hideBackground == null" class="logo-bg"></div>
+        </a>
+        `,
+        bindings: {
+            hideBackground: '@',
+            align: '@',
+        },
+        controllerAs: 'vm',
     });
