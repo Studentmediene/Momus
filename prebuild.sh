ddd8bae8534ad01a35c403ec0a9b501d8c6e1928
egrimstad <egrimstad95@gmail.com>
2018-10-15 12:19:28 +0200

    MOM-249 Fix prebuild to copy in sharepoint auth

--- prebuild.sh
+++ prebuild.sh
@@ -11,6 +11,8 @@ rm -rf target
 # copy in production values
 cp /srv/momus/local.properties src/main/resources/local.properties
 cp /srv/momus/Momuslive-6b43d0b81c8e.p12 src/main/resources/googlekey.p12
+cp /srv/momus/spcert.pem src/main/resources/spcert.pem
+cp /srv/momus/spkey.der src/main/resources/spkey.der
 
 # install frontend dependencies
 npm install
