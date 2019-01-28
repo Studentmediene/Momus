d1d124c0e20f0e6633624e39045bdf3e81bdf0bd
Christoffer Wilhelm Gran <cwgran@gmail.com>
2019-01-28 18:39:35 +0100

    MOM-264 Fix missing parameter in resource url

--- webapp/js/resources/Publication.js
+++ webapp/js/resources/Publication.js
@@ -18,7 +18,7 @@
 
 angular.module('momusApp.resources')
     .factory('Publication', (momResource) => {
-        return momResource('/api/publications/:id',
+        return momResource('/api/publications/:id/:resource',
             {
                 id: '@id'
             },
